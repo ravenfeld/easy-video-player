58deb75831df9a9d2e69fb1052444b4e778db095
Alexis Lecanu <alexis.lecanu@gmail.com>
2019-07-31 15:39:55 +0200

    add test startFullscreen

--- sample/src/main/java/com/ravenfeld/easyvideoplayersample/SimpleActivity.java
+++ sample/src/main/java/com/ravenfeld/easyvideoplayersample/SimpleActivity.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ravenfeld.easyvideoplayer.EasyVideoPlayerConfig;
+
 public class SimpleActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        EasyVideoPlayerConfig.setDebug(true);
         setContentView(R.layout.activity_main);
     }
 
